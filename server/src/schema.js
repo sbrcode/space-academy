ef7561a2a8a1c069b4cc92fa6047fd19715eceb3
SylvainBR <3095440-Sylvainn7@users.noreply.gitlab.com>
2022-07-06 15:13:26 +0200

    mod-schema-patch

--- server/src/schema.js
+++ server/src/schema.js
@@ -46,6 +46,21 @@ const typeDefs = gql`
     "The Module's length in minutes"
     length: Int
   }
+
+  type Mutation {
+    incrementTrackViews(id: ID!): IncrementTrackViewsResponse!
+  }
+
+  type IncrementTrackViewsResponse {
+    "Similar to HTTP status code, represents the status of the mutation"
+    code: Int!
+    "Indicates whether the mutation was successful"
+    success: Boolean!
+    "Human-readable message for the UI"
+    message: String!
+    "Newly updated track after a successful mutation"
+    track: Track
+  }
 `;
 
 module.exports = typeDefs;
